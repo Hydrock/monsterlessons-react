edc2c9ccef4a89be5e0353327d67d23345b23bfb
Aleksey <avechkanov@ourgapps.com>
2017-02-21 14:11:56 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import RegistrationForm from './RegistrationForm';
 
 import Block from './block';
-
+import Menu from './Menu';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Menu/>
         <RegistrationForm/>
         <Block/>
       </div>
--- src/block/index.js
+++ src/block/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+
+
 import '../App.css';
 
 export default class Block extends Component {
